cf16997ec233d057a423912a83146ba061834d90
WebDevolper1122 <sb2137842@gmail.com>
2022-08-16 23:53:58 +0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './main-portal/home/home.component';
+import { AboutComponent } from './main-portal/about/about.component';
+import { ContactComponent } from './main-portal/contact/contact.component';
+import { HeaderComponent } from './main-portal/header/header.component';
+import { FooterComponent } from './main-portal/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
